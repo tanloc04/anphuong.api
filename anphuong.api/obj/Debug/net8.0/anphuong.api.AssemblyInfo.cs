b7420c695f280911a2d4df9e465b32a49287ced6
thanhtien23233 <thanhtien23233@gmail.com>
2025-09-08 20:17:28 +0700

    initial

--- anphuong.api/obj/Debug/net8.0/anphuong.api.AssemblyInfo.cs
+++ anphuong.api/obj/Debug/net8.0/anphuong.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anphuong.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5dc038cef2c5f8281fbd7ecb3edef06e216a4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c1f191c5127a928e534f3fd357b9778e8433be")]
 [assembly: System.Reflection.AssemblyProductAttribute("anphuong.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anphuong.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
