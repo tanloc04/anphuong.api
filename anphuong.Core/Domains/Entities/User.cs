7995729363f6d5a8bbbc02327edfc60cbbcaa5db
thanhtien23233 <thanhtien23233@gmail.com>
2025-09-18 20:52:52 +0700

    update database

--- anphuong.Core/Domains/Entities/User.cs
+++ anphuong.Core/Domains/Entities/User.cs
@@ -3,11 +3,9 @@
     public class User : Entity
     {
         public string Username { get; set; }
-        public string? Password { get; set; }
         public string? PasswordHash { get; set; }
         public string? Email { get; set; }
         public string Status { get; set; }
-        public int CustomerId { get; set; }
         public Customer Customer { get; set; }
     }
 }
