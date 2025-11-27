cf4e15aa4e9850108893a302755d476ba7c2f1fa
thanhtien23233 <thanhtien23233@gmail.com>
2025-11-27 21:29:27 +0700

    Update DTO.cs

--- anphuong.Core/Domains/DTOs/StandardizedDTOs/DTO.cs
+++ anphuong.Core/Domains/DTOs/StandardizedDTOs/DTO.cs
@@ -9,11 +9,15 @@ namespace anphuong.Core.Domains.DTOs.StandardizedDTOs
 {
     public class DTO
     {
-        public string Id;
+        [JsonPropertyOrder(-100)]
+        public string Id { get; set; }
 
         [JsonPropertyName("createdAt")]
+        [JsonPropertyOrder(100)]
         public DateTime CreatedAt { get; set; }
+
         [JsonPropertyName("updatedAt")]
+        [JsonPropertyOrder(200)]
         public DateTime UpdatedAt { get; set; }
     }
 }
